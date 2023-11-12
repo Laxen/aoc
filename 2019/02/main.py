03634b1ce34d8a5969a3eaa3a718801b1da4b392
Alexander Ganslandt <alex.ganslandt@gmail.com>
2023-11-12 16:27:22 +0100

    2019/02: Part 2

--- 2019/02/main.py
+++ 2019/02/main.py
@@ -13,17 +13,31 @@ else:
 
 # -----------------
 
-for i in range(0, len(data), 4):
-    print(i)
-    code = data[i]
-    match code:
-        case 1:
-            data[data[i+3]] = data[data[i+1]] + data[data[i+2]]
-        case 2:
-            data[data[i+3]] = data[data[i+1]] * data[data[i+2]]
-        case 99:
-            print("EXIT")
-            print(data)
-            exit()
-        case _:
-            print("code:", code)
+def run(_data):
+    for i in range(0, len(_data), 4):
+        print(i)
+        code = _data[i]
+        match code:
+            case 1:
+                _data[_data[i+3]] = _data[_data[i+1]] + _data[_data[i+2]]
+            case 2:
+                _data[_data[i+3]] = _data[_data[i+1]] * _data[_data[i+2]]
+            case 99:
+                if _data[0] == 19690720:
+                    print("EXIT")
+                    print(100*i1+i2)
+                    exit()
+                else:
+                    return
+            case _:
+                print("code:", code)
+                exit()
+
+for i1 in range(0, 99):
+    for i2 in range(0, 99):
+        data[1] = i1
+        data[2] = i2
+
+        print(data)
+        run(data.copy())
+
