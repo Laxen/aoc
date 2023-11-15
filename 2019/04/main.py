46ef6d29812333d1908c53776eccba44158dbdc7
Alexander Ganslandt <alex.ganslandt@gmail.com>
2023-11-15 19:08:07 +0100

    2019/04: Part 2

--- 2019/04/main.py
+++ 2019/04/main.py
@@ -15,13 +15,15 @@ else:
 # -----------------
 
 def is_valid(num):
-    adjacent = False
+    counter = {}
     for i in range(len(num)):
+        if num[i] not in counter:
+            counter[num[i]] = 1
+        else:
+            counter[num[i]] += 1
         if i > 0 and num[i] < num[i - 1]:
             return False
-        if i > 0 and num[i] == num[i - 1]:
-            adjacent = True
-    return adjacent
+    return 2 in counter.values()
 
 count = 0
 for i in range(246515, 739105 + 1):
