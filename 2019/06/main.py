df32fe955bfb1f75cde481612be3f2676d98477b
Alexander Ganslandt <alex.ganslandt@gmail.com>
2023-11-18 16:57:59 +0100

    2019/06: Part 2

--- 2019/06/main.py
+++ 2019/06/main.py
@@ -17,8 +17,9 @@ else:
 # -----------------
 
 class Body:
-    def __init__(self):
+    def __init__(self, name):
         self.orbits = []
+        self.name = name
 
     def add_orbit(self, body):
         self.orbits.append(body)
@@ -29,19 +30,42 @@ class Body:
             orbits += body.n_orbits()
         return orbits
 
+    def orbit_body(self):
+        body = self.orbits[0]
+        return body
+
+    def orbit_dict(self):
+        ret = dict()
+        cnt = 0
+        body = self.orbit_body()
+        while body.name != "COM":
+            ret[cnt] = body
+            cnt += 1
+            body = body.orbit_body()
+        return ret
+
 bodies = {}
 
 for line in data:
     body1, body2 = line.split(")")
 
     if body1 not in bodies:
-        bodies[body1] = Body()
+        bodies[body1] = Body(body1)
     if body2 not in bodies:
-        bodies[body2] = Body()
+        bodies[body2] = Body(body2)
 
     bodies[body2].add_orbit(bodies[body1])
 
-sum_orbits = 0
-for name, body in bodies.items():
-    sum_orbits += body.n_orbits()
-print(sum_orbits)
+you_dict = bodies["YOU"].orbit_dict()
+san_dict = bodies["SAN"].orbit_dict()
+
+tot_dist = 0
+for you_dist, you_body in you_dict.items():
+    for san_dist, san_body in san_dict.items():
+        if you_body == san_body:
+            print(you_body.name)
+            print("you dist:", you_dist)
+            print("san dist:", san_dist)
+            tot_dist = you_dist + san_dist
+            print("tot_dist", tot_dist)
+            exit()
