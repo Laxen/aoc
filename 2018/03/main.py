d932367a35b9b01be70daf7dd91137a966b84376
Alexander Ganslandt <alex.ganslandt@gmail.com>
2023-11-27 20:37:48 +0100

    2018/03: Part 1

---
+++ 2018/03/main.py
@@ -0,0 +1,55 @@
+import sys
+import numpy as np
+from pyhelpers import Parser
+
+np.set_printoptions(linewidth=np.inf)
+
+def make_data(input_file):
+    return Parser.ints_to_list(input_file)
+
+if len(sys.argv) > 1:
+    print("INPUT\n")
+    data = make_data("input")
+else:
+    print("EXAMPLE\n")
+    data = make_data("example")
+
+# -----------------
+
+class Claim:
+    def __init__(self, id, left, top, width, height):
+        self.id = id
+        self.left = left
+        self.top = top
+        self.width = width
+        self.height = height
+
+        self.map = dict()
+        for x in range(self.left, self.left + self.width):
+            for y in range(self.top, self.top + self.height):
+                self.map[(y, x)] = 1
+
+    def merge_map(self, map):
+        for c, _ in self.map.items():
+            if c in map:
+                map[c] += 1
+            else:
+                map[c] = 1
+
+    def __repr__(self):
+        return f"{self.id}"
+
+claims = []
+for i in range(0, len(data), 5):
+    claims.append(Claim(*data[i:i+5]))
+
+map = dict()
+for claim in claims:
+    claim.merge_map(map)
+
+count = 0
+for c, v in map.items():
+    if v > 1:
+        count += 1
+
+print(count)
