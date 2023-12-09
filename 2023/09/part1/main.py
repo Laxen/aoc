c437241247d88304fd21a2b2264c8b6c14755584
Alexander Ganslandt <alexanga@axis.com>
2023-12-09 06:38:51 +0100

    2023/09: Diffs on Diffs
    Change-Id: I79c9e9f6b48e2492654d6bfeeebe14ab4553ec58

---
+++ 2023/09/part1/main.py
@@ -0,0 +1,60 @@
+import sys, re, time
+import numpy as np
+from collections import defaultdict, deque
+from pyhelpers import Parser
+import pprint
+
+pp = pprint.PrettyPrinter(width=200).pprint
+
+np.set_printoptions(linewidth=np.inf)
+
+def make_data(input_file):
+    ret = []
+    with open(input_file, "r") as f:
+        for line in f:
+            m = re.findall(r"-?\d+", line)
+            ret.append([int(d) for d in m])
+    return ret
+
+if len(sys.argv) > 1:
+    print("INPUT\n")
+    data = make_data("input")
+else:
+    print("EXAMPLE\n")
+    data = make_data("example")
+
+# -----------------
+
+diffs = []
+s = 0
+
+for history in data:
+    allzero = False
+    diffs.append(history)
+
+    while not allzero:
+        allzero = True
+        diff = []
+
+        for i in range(1, len(diffs[-1]), 1):
+            val = diffs[-1][i] - diffs[-1][i-1]
+            diff.append(val)
+
+            if val != 0:
+                allzero = False
+
+        diffs.append(diff)
+
+    pp(diffs)
+    diffs.reverse()
+    goal = 0
+    for diff in diffs:
+        x = diff[-1] + goal
+        diff.append(x)
+        goal = x
+
+    s += goal
+
+    diffs = []
+
+print(s)
