b6724808344b4e7eaffede2aed88c86c417a3218
Alexander Ganslandt <alex.ganslandt@gmail.com>
2023-12-24 05:38:46 +0100

    template: Improve

--- template/main.py
+++ template/main.py
@@ -1,14 +1,21 @@
 import sys, re, time
 import numpy as np
 from collections import defaultdict, deque
-from pyhelpers import Parser
+from pyhelpers import Parser, in_bounds
 from pprint import pprint
+from heapq import heappush, heappop
 
 np.set_printoptions(linewidth=np.inf)
+#sys.setrecursionlimit(10000)
 
 def make_data(input_file):
+    ret = []
     with open(input_file, "r") as f:
+        for line in f:
+            line = line.strip()
+            ret.append(line)
         pass
+    return ret
 
 if len(sys.argv) > 1:
     print("INPUT\n")
